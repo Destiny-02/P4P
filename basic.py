6d6b1e46985401934073199f82e66867f7ecc54c
Kyle Hensel <k.y.l.e@outlook.co.nz>
2023-04-02 13:26:45 +1300

    link the analyser to the parser output

--- basic.py
+++ basic.py
@@ -1,3 +1,5 @@
+import json
+
 def txtToSet(filename):
     data = set()
     isFirst = True
@@ -10,18 +12,35 @@ def txtToSet(filename):
             isFirst = False
 
         words = line.lower().split()
-        for word in words: 
+        for word in words:
             data.add(word)
     return data
+
+def jsonToSet(filename: str) -> set:
+  data = set()
+  with open(filename, encoding="utf-8") as jsonFile:
+    parserOutputJson = json.load(jsonFile)
+
+    for item in parserOutputJson['identifiers']:
+      words: list[str] = item['name'].split("_") # we only support snake_case for this basic PoC
+      for word in words:
+        data.add(word)
+
+  return data
 
 
 # Main
 domain_terms = txtToSet('domain_terms.txt')
+
+
+# uncomment these lines to use the output from the parser
 identifiers = txtToSet('submission.txt')
+# identifiers = jsonToSet('./parsers/output/java.json')
+
 
 count = 0
 numDomainTerms = len(domain_terms)
 for term in domain_terms:
-    if term in identifiers:
-      count += 1
+  if term in identifiers:
+    count += 1
 print(count/numDomainTerms)
--- parsers/input/Sample.java
+++ parsers/input/Sample.java
@@ -4,11 +4,17 @@ public class Sample {
   /**
    * Sample is a class
    */
-  public int myMethod() {
+  public int get_annual_income_deduction() {
     int a = 1;
     int b = 5;
 
     // this is a comment
     return a + b;
+  }
+
+  private static final double paye_tax_bracket = 17.5;
+
+  protected String get_payslip_for_paye() {
+    return "";
   }
 }
