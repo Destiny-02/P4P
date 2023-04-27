fb5028d5858690b3cdd2d78abd314e34db3cc0ff
Destiny Li <dli393@aucklanduni.ac.nz>
2023-04-28 11:52:14 +1200

    Tool for stemming list of domain terms

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Setup
 
 - Install python3 or later
-- run `python -v` to confirm that the python version in PATH is v3
+- run `python --version` to confirm that the python version in PATH is v3
 - run `python -m pip install --user pipenv` to install [the package manager](https://packaging.python.org/en/latest/tutorials/managing-dependencies/)
 - run `npm install` equivilant command to install [the dependencies listed in Pipfile](./Pipfile)
 
---
+++ domain-terms-stemming/stem.py
@@ -0,0 +1,26 @@
+from porter2stemmer import Porter2Stemmer
+
+stemmer = Porter2Stemmer()
+
+# Read in input file
+with open('input.txt', 'r') as input_file:
+    input_terms = set(line.strip() for line in input_file if line.strip())
+
+
+# Stem input terms
+stemmed_terms = set(stemmer.stem(term) for term in input_terms)
+
+# Remove duplicates from stemmed terms
+output_terms = set(stemmed_terms)
+
+# Sort output terms in alphabetical order
+output_terms = sorted(output_terms)
+
+# Write to output file
+with open('output.txt', 'w') as output_file:
+    for term in output_terms:
+        output_file.write(term + '\n')
+
+# Print number of terms in input and output files
+print('Number of terms in input file:', len(input_terms))
+print('Number of terms in output file:', len(output_terms))
