6a354f3da4ac8d7094b1d5aeb4ce5ffd53155f10
Kyle Hensel <k.y.l.e@outlook.co.nz>
2023-05-10 00:14:27 +1200

    add another parser testcase

---
+++ src/parsers/tests/complex.py
@@ -0,0 +1,23 @@
+"""
+This is a block comment in the middle of nowhere
+
+it has a fake # inline comment within the block
+
+"""  # comment on the same line as the block end
+
+
+"""block comment above func - this is not valid"""
+
+
+def identifier1():
+    """block comment within func"""
+
+
+identifier2 = "hello"
+
+
+def identifier3():
+    pass
+
+
+tripleQuotedString = """ this is a normal string """
