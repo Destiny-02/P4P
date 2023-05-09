8260aa350a9d71b17eae43bdb8cc9a02bdf6bcdc
Destiny Li <dli393@aucklanduni.ac.nz>
2023-05-09 12:40:31 +1200

    write stats to csv

--- src/analyseCodebases.py
+++ src/analyseCodebases.py
@@ -3,13 +3,21 @@ from helper.conversion import (
 	txtToSet,
 	cleanSetOfTerms, removeSeenStemmed, setToTxt
 )
-from helper.io import findJavaFiles, setToSheet, findRepoPaths
+from helper.io import findJavaFiles, setToSheet, findRepoPaths, deleteFileIfExists
 from helper.splitting import splitIdentifiers
 from os import path
 import csv
 
 def getPath(relativePath):
   return path.join(path.dirname(__file__), relativePath)
+
+def writeResultsToCsv(designCounts, contextCounts, neitherCounts, csvName):
+  deleteFileIfExists(csvName)
+  with open(csvName, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Design", "Context", "Neither"])
+    for i in range(len(designCounts)):
+      writer.writerow([designCounts[i], contextCounts[i], neitherCounts[i]])
 
 def main(pathToData, domainFolderName):
   contextTerms = txtToSet(getPath("vocabularies/" + domainFolderName + "/context.txt"))
@@ -53,7 +61,4 @@ if __name__ == "__main__":
   Analyses all the repos for a domain and produces stats on the percentage of terms that are design, context or neither
   """
   (designCounts, contextCounts, neitherCounts, totalCounts) = main(getPath("../data/ugrad-009-01/"), "ugrad-009-01")
-  print(designCounts)
-  print(contextCounts)
-  print(neitherCounts)
-  print(totalCounts)
+  writeResultsToCsv(designCounts, contextCounts, neitherCounts, "ugrad-009-01-stats.csv")
--- src/vocabularlyBuilder.py
+++ src/vocabularlyBuilder.py
@@ -32,11 +32,11 @@ def saveTermsToBeCategorised(pathToData, domainFolderName):
 	newTerms = removeSeenStemmed(terms, combinedTerms)
 
 	# Write the terms to be categorised to a spreadsheet
-	setToSheet(newTerms, "toCategorise.csv")
+	setToSheet(newTerms, "to-categorise.csv")
 
 def saveCategoriseSheetToTxt(domainFolderName):
 	"""
-	Saves the toCategorise.csv spreadsheet containing terms that have been categorised as 
+	Saves the to-categorise.csv spreadsheet containing terms that have been categorised as 
 	context (c), design (d) or neither (n) to the context.txt, design.txt and neither.txt text files
 	"""
 	dSet = set()
@@ -44,7 +44,7 @@ def saveCategoriseSheetToTxt(domainFolderName):
 	nSet = set()
 	undefinedSet = set()
 
-	with open('toCategorise.csv', 'r') as file:
+	with open('to-categorise.csv', 'r') as file:
 		reader = csv.reader(file)
 		for row in reader:
 			word, letter = row
@@ -74,7 +74,7 @@ if __name__ == "__main__":
 	1. Pick a folder in the data folder that you want to categorise the identifiers for
 	2. Change the path in the call to saveTermsToBeCategorised to the path of the folder you picked
 	3. Run this file
-	4. Open the toCategorise.csv spreadsheet
+	4. Open the to-categorise.csv spreadsheet
 	5. For each term, decide whether it is a context term, a design term or neither
 	6. Enter the letter c, d or n in the second column of the spreadsheet
 	7. Save the spreadsheet
