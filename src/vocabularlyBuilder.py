89425d16d9c58a2402d6eae308816e62c34f90a3
Destiny Li <dli393@aucklanduni.ac.nz>
2023-05-13 17:01:02 +1200

    design1000

--- src/vocabularlyBuilder.py
+++ src/vocabularlyBuilder.py
@@ -148,6 +148,6 @@ if __name__ == "__main__":
 	10. context.txt, design.txt and neither.txt will be updated with the terms you categorised
 	"""
 	# saveTermsToBeDetermined(getPath("../data/ugrad-009-01/domain-description.txt"))
-	saveDomainSheetToTxt("ugrad-009-01")
+	# saveDomainSheetToTxt("ugrad-009-01")
 	# saveTermsToBeCategorised(getPath("../data/ugrad-009-01/design1000"), "ugrad-009-01")
 	# saveCategoriseSheetToTxt("ugrad-009-01")
