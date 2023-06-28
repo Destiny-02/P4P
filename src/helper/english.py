0124bd92102f43dd710057bb7992c82b1eacb92d
Destiny Li <dli393@aucklanduni.ac.nz>
2023-06-28 21:26:04 +1200

    Add back fixSpelling
    Might be useful for the feedback tool

--- src/helper/english.py
+++ src/helper/english.py
@@ -1,30 +1,59 @@
 import json
 from os import path
+import enchant
 
 def fixUSSpelling(word: str) -> str:
-    """
-    If the word is a word in US English, it returns the UK English spelling of the word.
-    Otherwise, it returns itself.
-    american_spellings.json must be in the data/downloaded folder
-    """
-    # If it is less than five characters and not a word, it is more likely that it is a abbreviation or acronym so we won't touch it
-    if len(word) < 5:
-        return word
+	"""
+	If the word is a word in US English, it returns the UK English spelling of the word.
+	Otherwise, it returns itself.
+	american_spellings.json must be in the data/downloaded folder
+	"""
+	# If it is less than five characters and not a word, it is more likely that it is a abbreviation or acronym so we won't touch it
+	if len(word) < 5:
+		return word
 
-    with open(getPath('../../data/downloaded/american_spellings.json')) as f:
-        spellings = json.load(f)
-    
-    britishSpelling = spellings.get(word)
-    if britishSpelling:
-        return cleanString(britishSpelling)
-    else:
-        return word
+	with open(getPath('../../data/downloaded/american_spellings.json')) as f:
+		spellings = json.load(f)
+	
+	britishSpelling = spellings.get(word)
+	if britishSpelling:
+		return cleanString(britishSpelling)
+	else:
+		return word
+	
+def fixSpelling(word: str, retUsedForDisplay=True) -> str:
+	"""
+	If the word is less than five characters, it returns the word.
+	If the word seems to be misspelt, it returns the corrected spelling.
+	Otherwise, it returns the word.
+	Note: if the word is misspelt, the function can sometimes return more than one word (separated by a space) 
+	so this function should only be used to display feedback to the user.
+	"""
+	if len(word) < 5:
+		return word
 
-def cleanString(term: str) -> str:
-  """
-  Converts a term to stripped lowercase English letters
-  """
-  return ''.join(char for char in term if char.isalpha()).lower().strip()
+	ukDict = enchant.Dict("en_GB")
+
+	if ukDict.check(word):
+		return word
+
+	suggestions = ukDict.suggest(word)
+	if suggestions and suggestions[0]:
+		print("Fixed spelling of " + word + " to " + suggestions[0])
+		return cleanString(suggestions[0], retUsedForDisplay)
+
+	return word
+   
+
+def cleanString(term: str, keepSpaces=False) -> str:
+	"""
+	Converts a term to stripped lowercase English letters
+	keepSpaces - If true, spaces are kept in the converted output string
+	"""
+	if keepSpaces:
+		return ''.join(char for char in term if char.isalpha() or char.isspace()).lower().strip()
+
+	return ''.join(char for char in term if char.isalpha()).lower().strip()
 
 def getPath(relativePath):
   return path.join(path.dirname(__file__), relativePath)
--- tests/test_english.py
+++ tests/test_english.py
@@ -5,7 +5,7 @@ import sys
 project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, project_dir)
 
-from src.helper.english import fixUSSpelling
+from src.helper.english import fixUSSpelling, fixSpelling
 
 def test_fixUSSpelling():
     assert fixUSSpelling("abc") == "abc" # Less than 5 characters
@@ -16,3 +16,11 @@ def test_fixUSSpelling():
     assert fixUSSpelling("color") == "colour" # US spelling
     assert fixUSSpelling("traveling") == "travelling" # US spelling
     assert fixUSSpelling("travelling") == "travelling" # UK spelling
+
+def test_fixSpelling():
+    assert fixSpelling("thr") == "thr" # Less than 5 characters
+    assert fixSpelling("chicken") == "chicken" # Correct spelling
+    assert fixSpelling("fosjepfosjeopfjse") == "fosjepfosjeopfjse" # Not a word
+    assert fixSpelling("chickennuggets") == "chicken nuggets" # Misspelt word (is two words)
+    assert fixSpelling("fnfex") == "fedex" # Detected as misspeling, converts to lowercase
+    assert fixSpelling("color") == "colour" # US spelling
